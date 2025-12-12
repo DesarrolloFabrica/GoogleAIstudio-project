0835112e915466aa7e0106ca1421dbe3212ccd7c
DesarrolloFabrica <desarrollofabrica@cun.edu.co>
2025-12-12 15:00:40 -0500

    api

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,8 +1,9 @@
 // src/services/apiClient.ts
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL ?? "http://localhost:3001"; 
-// AJUSTA este puerto al que realmente use tu backend Nest
+const API_BASE_URL = import.meta.env.VITE_API_URL ?? "http://localhost:3001";
+
+console.log("API_BASE_URL =", API_BASE_URL); // <-- esto para comprobar
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
